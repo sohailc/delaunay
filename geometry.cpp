1123bcae79fd8e86a64417bf68b2e9f4656a5c90
sohailc <schatoor@gmail.com>
2017-02-22 18:49:39 +0100

    No more compile errors

--- geometry.cpp
+++ geometry.cpp
@@ -3,64 +3,118 @@
 #include <vector>
 #include <stdlib.h>
 #include <stdio.h>
+#include <cstring>
 
 #include "linearLeastSquares.h"
 
 using namespace std;
 
-matrix vertices_to_column_matrix(vector<Vertex> columns, matrix *a){
- 
-    // We assume that memory for the matrix has been allocated 
-    int n_columns = columns.size();
-    for (i_col=0; i_col<n_columns; i_col++){
-     
-        column = columns[i_col];
-        n_row = column.p.size();
-        for (i_row=0; i_row<n_row; i_row++){
-            double element = column.p[i_row];
-            EL((*a), i_row, i_col) = element;
+
+class Vertex{
+
+    private:
+        vector<double> p;
+
+        Vertex element_wise_operation(Vertex a, Vertex b, double (op)(double, double)){
+            vector<double> result;
+            vector<double> :: iterator i, j;
+
+            for ( i=a.p.begin(), j=b.p.begin(); i<a.p.end(), j<b.p.end(); i++, j++){
+                result.push_back(op(*i, *j));
+            }
+
+            return Vertex(result);
+        }
+
+    public:
+        int ndim;
+        
+        Vertex operator-(Vertex a){
+            return element_wise_operation(p, a, [](double i, double j){return i - j;});
+        }
+
+        Vertex operator+(Vertex a){
+            return element_wise_operation(p, a, [](double i, double j){return i + j;});
+        }
+
+        Vertex(vector<double> a){
+            p = a;
         }
         
-    }
-}
+        double at(int i){
+            return p.at(i);
+        }
+
+        Vertex(int ndim_, ...){   // so we can initialize a vertex like Vertex p(ndim, x, y, z); 
+
+            double ai;
+            int i = 0;
+
+            va_list arguments;
+            va_start ( arguments, ndim_ );
+            for (i = 0; i<ndim_; i++ ){
+                ai = va_arg ( arguments, double );
+                p.push_back(ai);
+            }
+            va_end ( arguments );
+            ndim = ndim_;
+        }
+
+        void print(){
+            vector<double> :: iterator i;
+            for (i=p.begin(); i<p.end(); i++){
+                cout << *i << " ,";
+            }
+            cout << endl;
+        }
+
+};
+
 
 class Matrix{
   
     private:
         matrix data;
+        
+        int allocate_memory(){
+            NEW_MAT(data);
+        }
     
     public:
         
         int n_columns;
         int n_rows;
+        bool initialized = false;
         
         Matrix(vector<Vertex> columns){
             
             n_columns = columns.size();
-            n_rows = columns[0].p.size();
+            n_rows = columns[0].ndim;
             data.m = n_rows;
-            a.n = n_columns;
-            NEW_MAT(a);
+            data.n = n_columns;
+            if (allocate_memory() == EXIT_SUCCESS){
             
-            for (int i=0; i<n_columns; i++){
-                for(int j=0; j<n_rows; j++){
-                    EL(data, j, i) = columns[i].p[j];
+                for (int i=0; i<n_columns; i++){
+                    for(int j=0; j<n_rows; j++){
+                        EL(data, j, i) = columns[i].at(j);
+                    }
                 }
+                initialized = true;
             }
         }
         
         Matrix(Vertex column){
             
             n_columns = 1;
-            n_rows = column.p.size();
+            n_rows = column.ndim;
             data.m = n_rows;
-            a.n = n_columns;
-            NEW_MAT(a);
+            data.n = n_columns;
+            if (allocate_memory() == EXIT_SUCCESS){
             
-            for (int i=0; i<n_columns; i++){
                 for(int j=0; j<n_rows; j++){
-                    EL(data, j, i) = columns[i].p[j];
+                    EL(data, j, 0) = column.at(j);
                 }
+                initialized = true;
             }
         }
         
@@ -68,12 +122,12 @@ class Matrix{
          
             data.m = a.m;
             data.n = a.n;
-            NEW_MAT(data);
-            
-            memcpy(&data, &a, sizeof data);
-            n_columns = a.n;
-            n_rows = a.m;
-            
+            if (allocate_memory() == EXIT_SUCCESS){
+                memcpy(&data, &a, sizeof data);
+                n_columns = a.n;
+                n_rows = a.m;
+                initialized = true;
+            }
         }
         
         ~Matrix(){
@@ -83,12 +137,10 @@ class Matrix{
         Matrix operator/(Matrix b){
             
             matrix x; 
-            Matrix result;
-            
-            x.m = n_rows
+            x.m = n_rows;
             x.n = b.data.n;
             
-            linearLeastSquares(data, b, &x);
+            linearLeastSquares(data, b.data, &x);
             return Matrix(x);
         }
         
@@ -115,59 +167,6 @@ class Matrix{
         }
 };
 
-class Vertex{
-
-    private:
-        vector<double> p;
-
-        Vertex element_wise_operation(Vertex a, Vertex b, double (op)(double, double)){
-            vector<double> result;
-            vector<double> :: iterator i, j;
-
-            for ( i=a.p.begin(), j=b.p.begin(); i<a.p.end(), j<b.p.end(); i++, j++){
-                result.push_back(op(*i, *j));
-            }
-
-            return Vertex(result);
-        }
-
-    public:
-        Vertex operator-(Vertex a){
-            return element_wise_operation(p, a, [](double i, double j){return i - j;});
-        }
-
-        Vertex operator+(Vertex a){
-            return element_wise_operation(p, a, [](double i, double j){return i + j;});
-        }
-
-        Vertex(vector<double> a){
-            p = a;
-        }
-
-        Vertex(int ndim, ...){   // so we can initialize a vertex like Vertex p(ndim, x, y, z); 
-
-            double ai;
-            int i = 0;
-
-            va_list arguments;
-            va_start ( arguments, ndim );
-            for (i = 0; i<ndim; i++ ){
-                ai = va_arg ( arguments, double );
-                p.push_back(ai);
-            }
-            va_end ( arguments );
-        }
-
-        void print(){
-            vector<double> :: iterator i;
-            for (i=p.begin(); i<p.end(); i++){
-                cout << *i << " ,";
-            }
-            cout << endl;
-        }
-
-};
-
 class tetrahedron{ // a generalized tetrahedron in n-space
     vector<Vertex> vertices;
     int ndim;
@@ -182,7 +181,7 @@ class tetrahedron{ // a generalized tetrahedron in n-space
 
             int n_vertices = vertices.size();
             vector<Vertex> matrix_columns;
-            bool does_contain = true
+            bool does_contain = true;
             
             for (int i=0; i<n_vertices-1; i++){
                 
@@ -195,18 +194,16 @@ class tetrahedron{ // a generalized tetrahedron in n-space
                 // Solve matrix_columns * x = p - p0 for x
                 Matrix x = Matrix(p - p0) / Matrix(matrix_columns);
                 // return false if any(x) > 1
-                does_contain = x.any([](double v){return v > 1 ? false: true});
+                does_contain = x.any([](double v){return v > 1 ? false: true;});
                 
+                if (!does_contain) // no need for further checking 
+                    break;
             }
-            if (!does_contain) // no need for further checking 
-                break;
+        
+            return does_contain;
         }
-
-        free(a.data);
-        return does_contain;
-    }
-
 };
+
 
 int main(){
 
